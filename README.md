2bbfdd48eb4399fb86582bab2251d957e9d0367e
Maarten van Schaik <maarten@informaarten.nl>
2024-02-11 17:02:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Script for DNS-01 auth hook for Transip
+
+This is a simple python script to help creating and cleaning up a TXT
+record for validating a domain using DNS-01 using certbot. It can be
+used for `--manual-auth-hook` and --manual-cleanup-hook`.
+
+It is designed to not have any dependencies, it should work with any recent version
+of python3.
+
+Example usage:
+
+auth-transip.sh
+```
+certbot-dns-transip.sh \
+    create $CERTBOT_DOMAIN \
+    --username=<my username> \
+    --private_keyfile=<private.key> \
+    --validation="$CERTBOT_VALIDATION"
+```
+
+cleanup-transip.sh
+```
+TOKEN=$(echo $CERTBOT_AUTH_OUTPUT | awk '/JWT:/{print $2}')
+certbot-dns-transip.sh \
+    cleanup $CERTBOT_DOMAIN \
+    --bearer_token="$TOKEN"
+```
+
+Certbot command
+```
+certbot certonly --manual --preferred_challenges=dns \
+    --manual-auth-hook=auth-transip.sh \
+    --manual-cleanup-hook=cleanup-transip.sh \
+    --domain=some.domain.com
+```
+
---
+++ certbot-dns-transip.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+
+import argparse
+import base64
+import json
+import logging
+import secrets
+import subprocess
+import sys
+
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+
+class TransipClient:
+
+    API_HOST = 'api.transip.nl'
+    AUTH_URL = '/v6/auth'
+    DNS_URL = '/v6/domains/%s/dns'
+
+    """
+    Simple REST-client for Transip, supporting authentication, creating and
+    removing of TXT DNS records.
+    """
+    def __init__(self, username=None, keyfile=None, token=None):
+        if token is None and (username is None or keyfile is None):
+            raise ValueError("No token, username or keyfile provided")
+
+        self._username = username
+        self._keyfile = keyfile
+        self._token = token
+        self._logger = logging.getLogger()
+
+    def _signature(self, request):
+        json_req = json.dumps(request).encode()
+        proc = subprocess.Popen(["openssl","dgst","-sha512","-sign",self._keyfile],
+                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        sig = proc.communicate(input=json_req)[0]
+        return base64.b64encode(sig)
+
+    def _get_auth_headers(self):
+        if self._token is None:
+            self._token = self._get_token()
+            # Print JWT token to stdout so it can be captured for later use.
+            print("JWT: %s" % self._token)
+        return {
+                'Authorization': 'Bearer %s' % self._token
+        }
+
+    def _get_token(self):
+        self._logger.info("Authenticating.")
+        request = {
+                "login": self._username,
+                "nonce": secrets.token_urlsafe()[:32],
+                "read_only": False,
+                "expiration_time": "30 minutes",
+                "global_key": False,
+        }
+        headers = {
+                'Signature': self._signature(request)
+        }
+        return self._rest_call('POST', self.AUTH_URL,
+                               request, headers)['token']
+
+    def _rest_call(self, method, url, body=None, extra_headers=None):
+        json_req = json.dumps(body).encode()
+        headers = {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Accept': 'application/json',
+        } | (extra_headers if extra_headers is not None else {})
+
+        request = Request(url, data=json_req, headers=headers, method=method)
+        try:
+            with urlopen(request) as response:
+                if response.status == 204:
+                    return None
+                return json.loads(response.read().decode(
+                    response.headers.get_content_charset('utf-8')))
+        except HTTPError as e:
+            self._logger.error(e.fp.read())
+            raise
+
+    def _get_txt_record(self, domain, name):
+        resp = self._rest_call(
+                'GET', self.DNS_URL % domain,
+                extra_headers=self._get_auth_headers())
+        try:
+            return next(entry
+                        for entry in resp['dnsEntries']
+                        if entry['name'] == name and entry['type'] == 'TXT')
+        except StopIteration:
+            return None
+
+    def ensure_txt_record(self, domain, name, value):
+        existing_record = self._get_txt_record(domain, name)
+        if existing_record is None:
+            self._logger.info("Creating record for %s.%s" % (name, domain))
+        else:
+            self._logger.info("Updating record for %s.%s" % (name, domain))
+
+        record = {
+                'dnsEntry': {
+                    'name': name,
+                    'expire': 60,  # 1 minute is minimum.
+                    'type': 'TXT',
+                    'content': value,
+                }
+        }
+        self._rest_call(
+                'POST' if existing_record is None else 'PATCH',
+                self.DNS_URL % domain,
+                body=record, extra_headers=self._get_auth_headers())
+
+    def remove_txt_record(self, domain, name):
+        rec = self._get_txt_record(domain, name)
+        if rec is not None:
+            self._logger.info("Removing record %s.%s" % (name, domain))
+            body = {
+                    'dnsEntry': rec
+            }
+            self._rest_call(
+                    'DELETE',
+                    self.DNS_URL % domain,
+                    extra_headers=self._get_auth_headers(),
+                    body=body)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)s:%(message)s')
+
+    parser = argparse.ArgumentParser(
+            description='Manage TXT records for automatic certbot DNS-01 validation at Transip')
+    parser.add_argument('command', choices=('create', 'cleanup'),
+                        help='Create a record or clean it up')
+    parser.add_argument('domain',
+                        help='Domain to validate, e.g. value of CERTBOT_DOMAIN')
+    parser.add_argument('--validation',
+        help='Validation string, e.g. value of CERTBOT_VALIDATION')
+    parser.add_argument('--username', help='Username at Transip')
+    parser.add_argument('--private_keyfile',
+                        help='Name of file containing private key for Transip API')
+    parser.add_argument('--bearer_token', help='Existing bearer token for Transip')
+
+    args = parser.parse_args()
+    if (args.private_keyfile is None or args.username is None) and args.bearer_token is None:
+        logging.error("Provide either --bearer_token or --username and --private_keyfile to authenticate requests to Transip.")
+        sys.exit(1)
+
+    if args.command == 'create' and args.validation is None:
+        logging.error("Provide --validation to create record")
+        sys.exit(1)
+
+    # Only supports domains with a single dot in it. :-)
+    parts = args.domain.split('.')
+    domain = '.'.join(parts[-2:])
+    name = '.'.join(parts[:-2])
+
+    t = TransipClient(username=args.username, keyfile=args.private_keyfile,
+                      token=args.bearer_token)
+    if args.command == 'create':
+        t.ensure_txt_record(domain, name, args.validation)
+    elif args.command == 'cleanup':
+        t.remove_txt_record(domain, name)
+
